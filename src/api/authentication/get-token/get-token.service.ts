a4e641c7999173599706ecee36fb15c89abeb9d8
karimadio50 <karim.adio@gosmartdining.com>
2026-05-04 13:06:47 +0100

    feat: add authentication service and migrate to updated version 2 mock data schema

--- src/api/authentication/get-token/get-token.service.ts
+++ src/api/authentication/get-token/get-token.service.ts
@@ -1,14 +1,13 @@
+import { ToastPosDataService } from '@core/services/toast-pos-data.service';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Injectable()
 export class GetTokenService {
+  constructor(private readonly toastPosDataService: ToastPosDataService) { }
+
   execute(): any {
-    const accessPath = path.resolve(process.cwd(), 'src/mock-data/ToastMockData/access.json');
-    if (fs.existsSync(accessPath)) {
-      return JSON.parse(fs.readFileSync(accessPath, 'utf8'));
-    }
-    return { token: { accessToken: "MOCK_TOKEN" } };
+    return this.toastPosDataService.getAccount();
   }
 }
